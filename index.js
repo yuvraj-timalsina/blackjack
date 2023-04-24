bd994dfa741331bb2fef6b54d7ea2eb69d220420
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-04-24 18:18:58 +0545

    Special Cards Logic

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function getRandomCard() {
-    return Math.floor(Math.random() * 13) + 1
+    let randomCard = Math.floor(Math.random() * 13) + 1
+
+    if (randomCard > 10) {
+        return 10
+    } else if (randomCard === 1) {
+        return 11
+    }
+    return randomCard
 }
+
 function startGame() {
     renderGame()
 }
