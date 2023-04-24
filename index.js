1ba4352a39905ac79cb88a38e31959410cbb85bf
yuvraj-timalsina <timalsinayuvraj@gmail.com>
2023-04-24 18:58:20 +0545

    Finish

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let player = {
+    name: "Player", chips: 145
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,10 +9,6 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
-let player = {
-    name: "Per", chips: 145
-}
 
 let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": $" + player.chips
